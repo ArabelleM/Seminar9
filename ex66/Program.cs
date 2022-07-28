65d3c77e480a98ba2a4557f08147ad8b7c489c0f
Arabelle <yulitoniy@gmail.com>
2022-07-28 19:32:17 +0300

    ex66 rec

--- ex66/Program.cs
+++ ex66/Program.cs
@@ -7,9 +7,11 @@ Console.Write("Enter M: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter N: ");
 int n = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
-while (m <= n) {
-    sum = sum + m;
-    m++;
+
+int RecSum (int m1, int n1) {
+    if (m1 == n1) return m1;
+    else return n1 + RecSum(m1, n1 - 1);
+    
 }
-Console.WriteLine(sum);
+
+Console.WriteLine(RecSum(1, 11));
